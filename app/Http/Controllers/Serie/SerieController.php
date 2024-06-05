a3fcab0862d78245563e36143433d837dcc60509
gu1lhermeb <guilhermebraz089@gmail.com>
2024-06-04 23:12:19 -0300

    feat: creating controller and routes(index and store)

---
+++ app/Http/Controllers/Serie/SerieController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Serie;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SerieController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function store()
+    {
+        
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Serie\SerieController;
+
+Route::group(['prefix' => 'series'], function() {
+    Route::get('/', [SerieController::class, 'index']);
+    Route::post('/', [SerieController::class, 'store']);
+});
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
