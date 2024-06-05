f06d76f9b83875f8803eae25a4b02e984d631d13
gu1lhermeb <guilhermebraz089@gmail.com>
2024-06-05 16:06:33 -0300

    feat: adding delete route

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,7 @@ Route::get('/series', [SerieController::class, 'index']);
 Route::get('/series/{id}', [SerieController::class, 'showById']);
 Route::post('/series', [SerieController::class, 'store']);
 Route::put('/series/{id}', [SerieController::class, 'update']);
+Route::delete('/series/{id}', [SerieController::class, 'destroy']);
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
